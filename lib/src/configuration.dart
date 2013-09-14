e1ef2e3dfd76e1d651e44e6362219237adbba8a6
theburningmonk <theburningmonk@gmail.com>
2013-09-14 23:51:20 +0100

    - dialled down difficulty

--- lib/src/configuration.dart
+++ lib/src/configuration.dart
@@ -2,13 +2,13 @@ part of run_taotie_run;
 
 class Configuration {
   static const int    NUM_OF_TAOTIES        = 5;
-  static const double INIT_MIN_STARIUM_TIME = 5.0;
-  static const double INIT_MAX_STARIUM_TIME = 8.0;
+  static const double INIT_MIN_STARIUM_TIME = 6.0;
+  static const double INIT_MAX_STARIUM_TIME = 9.0;
 
   static const int    TAOTIE_MOVE_DELAY     = 100;
-  static const int    STARIUM_SPAWN_FREQ    = 3;  // how frequently (seconds) to spawn new starium
-  static const int    STARIUM_SHOWER_FREQ   = 8;  // how frequently (seconds) a new shower starts
-  static const int    STARIUM_SPEED_UP_FREQ = 12; // how frequently (seconds) stariums speed up
+  static const int    STARIUM_SPAWN_FREQ    = 4;  // how frequently (seconds) to spawn new starium
+  static const int    STARIUM_SHOWER_FREQ   = 10; // how frequently (seconds) a new shower starts
+  static const int    STARIUM_SPEED_UP_FREQ = 15; // how frequently (seconds) stariums speed up
 
   // from highest (most inner parts of screen) to lowest (most outter parts of screen)
   static ScoreZone _tier1 = new ScoreZone(5, 200, 150, Color.Red)
