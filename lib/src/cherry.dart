f8c747ec2df28169897f8896abdccc93554571c4
theburningmonk <theburningmonk@gmail.com>
2013-09-18 18:08:19 +0100

    - improved hit testing

--- lib/src/cherry.dart
+++ lib/src/cherry.dart
@@ -4,8 +4,18 @@ class Cherry extends Sprite {
   ResourceManager _resourceManager;
   int score;
 
+  static Rectangle _hitArea = new Rectangle(0, 22, 48, 51);
+
   Cherry(this._resourceManager, this.score) {
     new Bitmap(_resourceManager.getBitmapData("cherry"))
       ..addTo(this);
+  }
+
+  // use a rectangular bound to cover just the cherry meat
+  Rectangle getBounds(DisplayObject targetSpace) {
+    var rect = new Rectangle.from(_hitArea);
+    rect.offset(x, y);
+
+    return rect;
   }
 }
--- lib/src/taotie.dart
+++ lib/src/taotie.dart
@@ -5,6 +5,8 @@ class Taotie extends Sprite {
 
   ResourceManager _resourceManager;
   bool isBoss;
+  static Point _hitCenter = new Point(38, 38);
+  static int _hitRadius = 28;
 
   Taotie(this._resourceManager, [ bool this.isBoss = false ]) {
     new Bitmap(_resourceManager.getBitmapData(Characters.TAOTIE))
@@ -19,10 +21,32 @@ class Taotie extends Sprite {
   static Stream<HitEvent> get onHit => _onHitController.stream;
 
   void hitTest(List<Starium> stariums) {
-    var hitStarium = stariums.firstWhere((starium) => starium.hitTestObject(this), orElse : () => null);
+    var hitStarium = stariums.firstWhere((starium) => this.hitTestObject(starium), orElse : () => null);
 
     if (hitStarium != null) {
       _onHitController.add(new HitEvent(this, hitStarium));
     }
+  }
+
+  /// override the hitTestObject to use a circle in the middle for hit test purposes
+  bool hitTestObject(DisplayObject other) {
+    if (other.stage == null) return false;
+
+    var otherBounds = other.getBounds(stage);
+
+    if (otherBounds.left   <= x &&
+        otherBounds.top    <= y &&
+        otherBounds.right  >= x + width  &&
+        otherBounds.bottom >= y + height) {
+      return true;
+    }
+
+    var center = new Point(x, y).add(_hitCenter);
+    bool isHit (Point p) => p.distanceTo(center) <= _hitRadius;
+
+    return isHit(otherBounds.topLeft) ||
+           isHit(otherBounds.bottomRight) ||
+           isHit(otherBounds.topLeft.add(new Point(0, otherBounds.height))) ||
+           isHit(otherBounds.bottomRight.subtract(new Point(0, otherBounds.height)));
   }
 }
